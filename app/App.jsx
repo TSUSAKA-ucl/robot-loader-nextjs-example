26a6b8a1f20c847fa99d999d5db7004b1e034f4d
Yuji Tsusaka <tsusaka@ucl.nuee.nagoya-u.ac.jp>
2026-01-08 13:23:57 +0900

    robot-loader ver.1.0.17, ik-cd-worker ver.0.2.19に対応

--- app/App.jsx
+++ app/App.jsx
@@ -10,11 +10,9 @@ import '@ucl-nuee/robot-loader/vrControllerThumbMenu.js';
 import '@ucl-nuee/robot-loader/axesFrame.js';
 
 function App() {
-  const menuItems1 = "ur5e,ur5e,ur5e,ray";
   const deg90 = Math.PI/2;
   return (
-    <a-scene xr-mode-ui="enable: true"
-    >
+    <a-scene xr-mode-ui="XRMode: xr" >
       <a-entity camera position="-0.5 1.2 1.2"
       		wasd-controls="acceleration: 20; maxSpeed: 0.05; fly: true"
                 look-controls></a-entity>
@@ -22,18 +20,15 @@ function App() {
                 robot-registry >
         <a-entity right-controller
                   laser-controls="hand: right"
-                  raycaster="objects: .clickable"
-                  line="color: blue; opacity: 0.75"
-                  thumbstick-menu={`items: ${menuItems1}; laser: false`}
-                  thumbmenu-event-handler
-                  target-selector
+                  thumbstick-menu="items: ray; laser: false"
+                  target-selector="id: ur5e"
                   event-distributor
-                  visible="true">
+                  visible="false">
           <a-entity a-axes-frame="length: 0.1" />
         </a-entity>
       </a-entity>
       <a-plane id="ur5e"
-               position="0.0 0.5 0.0" rotation="-90 0 90"
+               position="0.0 0.0 -1.0" rotation="-90 0 -90"
                width="0.04" height="0.04" color="blue"
                robot-loader="model: ur5e"
                ik-worker={`0, ${-deg90}, ${deg90}, 0, ${deg90}, 0`}
